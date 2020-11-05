8d6ebccb57913f40bbf04f36baf2d89362ef2a96
jojo <yza442@sfu.ca>
2020-11-05 09:47:00 -0800

    fix content

--- js/main.js
+++ js/main.js
@@ -8,16 +8,16 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-function visibleCheckOut(n,m) {
-    prompt(working);
-    // alert(document.querySelector(".checkout" + n).style);
-    document.querySelector(".checkout" + n).style.visibility = "visible";
+// function visibleCheckOut(n,m) {
+//     prompt(working);
+//     // alert(document.querySelector(".checkout" + n).style);
+//     document.querySelector(".checkout" + n).style.visibility = "visible";
 
-    document.querySelector(".checkBtn-"+m).style.display = "none";
-    document.querySelector(".checkBtn-"+(m+1)).style.display = "inline-block";
-    setTimeout(function(){ 
-        document.querySelector(".checkBtn-"+m).style.display = "none";
-        document.querySelector(".checkBtn-"+(m+1)).style.display = "none";
-        document.querySelector(".checkBtn-"+(m+2)).style.display = "inline-block";
-    }, 1000);
-}
+//     document.querySelector(".checkBtn-"+m).style.display = "none";
+//     document.querySelector(".checkBtn-"+(m+1)).style.display = "inline-block";
+//     setTimeout(function(){ 
+//         document.querySelector(".checkBtn-"+m).style.display = "none";
+//         document.querySelector(".checkBtn-"+(m+1)).style.display = "none";
+//         document.querySelector(".checkBtn-"+(m+2)).style.display = "inline-block";
+//     }, 1000);
+// }
